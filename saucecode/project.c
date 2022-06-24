b9186c33308f911f12057fc3b08b7329a2731613
h0vno <w65557@student.wsiz.edu.pl>
2022-06-24 15:49:08 +0200

    return test func

--- saucecode/project.c
+++ saucecode/project.c
@@ -68,6 +68,38 @@ void TopDownMerge(int A[], int iBegin, int iMiddle, int iEnd, int B[])
     }
 }
 
+bool test()
+{
+    int *array, *array2, n, x, count = 0;
+
+    for (int i = 0; i < TEST_NUM; i++) {
+        n = rand() % ARR_SIZE + 1;
+        if (N)
+            n = N;
+        array = rand_array(n, MAX_SIZE);
+        array2 = copy_array(array, n);
+
+        mergesortfull(array2, n);
+        /* sssort(array2, n); */
+
+        if (!testsort(array, array2, n, DEBUG))
+            count++;
+
+        // test search
+        /* x = rand() % n; */
+        /* if(linear_search(array2[x], array2, n) == -1) { */
+        /* if(!binary_search(array2[x], array2, n)) { */
+        /*     printf("\nsearch for array[%d] == %d failed\n", x, array2[x]); */
+        /*     print_array(array2, n); */
+        /*     count++; */
+        /* } */
+
+        free(array);
+        free(array2);
+    }
+
+    printf("count: %d\n", count);
+}
 
 bool testsort(int *array, int *array_sorted, int n, bool print_succesful)
 {
